c643a6689407230efed52ecd34a7076a6771a151
bplok20010 <nobo.zhou@foxmail.com>
2019-03-19 23:53:38 +0800

    Git add

--- examples/demos/demo1.js
+++ examples/demos/demo1.js
@@ -1,50 +1,21 @@
 import React, { Component } from 'react';
-import {
-    Layout,
-    Header,
-    Content,
-    Footer,
-    Sider
-} from '../../src/index';
-
+import Input from '../../src/index';
 
 export default class DEMO extends Component {
 
     render() {
         return (
             <div>
-                <Layout>
-                    <Header>Header</Header>
-                    <Content>Content</Content>
-                    <Footer>Footer</Footer>
-                </Layout>
-                <br />
-                <Layout>
-                    <Header>Header</Header>
-                    <Layout>
-                        <Sider>Sider</Sider>
-                        <Content>Content</Content>
-                    </Layout>
-                    <Footer>Footer</Footer>
-                </Layout>
-                <br />
-                <Layout>
-                    <Header>Header</Header>
-                    <Layout>
-                        <Content>Content</Content>
-                        <Sider>Sider</Sider>
-                    </Layout>
-                    <Footer>Footer</Footer>
-                </Layout>
-                <br />
-                <Layout>
-                    <Sider>Sider</Sider>
-                    <Layout>
-                        <Header>Header</Header>
-                        <Content>Content</Content>
-                        <Footer>Footer</Footer>
-                    </Layout>
-                </Layout>
+                <Input size="small" placeholder="small size" />
+                <Input />
+                <Input size="large" />
+                <Input size="large" disabled placeholder="small large" value="test..." />
+                <Input prefix="ios-contact" placeholder="Enter name" style={{
+                    width: 'auto'
+                }} />
+                <Input suffix="ios-search" placeholder="Enter text" style={{
+                    width: 'auto'
+                }} />
             </div >
         );
     }
